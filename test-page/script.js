16d85948805c2d615a30046efe5ebb7a853b9b9e
Alyona Shmeleva <aki.tayn@gmail.com>
2019-09-19 05:00:07 +0300

    test-page modal window
    add modal window on desktop

--- test-page/script.js
+++ test-page/script.js
@@ -15,4 +15,25 @@ $(document).ready(function () {
       $('.name').hide();
     }
   }).resize();
+  var overlay = $('.overlay');
+  var openModal = $('.header__enter-mob');
+  var close = $('.enter-window__cancel-btn, .overlay');
+  var modal = $('.enter-window');
+  openModal.click(function () {
+    overlay.fadeIn(150, function () {
+      overlay.css('display', 'block');
+      modal.css('display', 'block');
+    });
+  });
+  close.click(function () {
+      modal // все мoдaльные oкнa
+        .fadeOut( 150,
+          function () {
+            modal.css('display', 'none');
+            overlay.fadeOut('slow');
+            overlay.css('display', 'none');
+          });
+    }
+
+  );
 });
